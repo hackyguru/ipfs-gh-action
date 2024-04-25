45819d2396ecaf1dfd201e908f03a4eb02e9f4b5
Guru <19eucs071@skcet.ac.in>
2024-04-25 16:44:50 +0530

    Create backup.js

---
+++ backup.js
@@ -0,0 +1,19 @@
+import lighthouse from '@lighthouse-web3/sdk'
+
+async function main() {
+
+  const apiKey = process.env.LIGHTHOUSE_API_KEY;
+  const uploadResponse = await lighthouse.upload(
+  './', 
+  apiKey
+);
+
+  const cid = uploadResponse.data.Hash;
+  console.log(`Uploaded to IPFS : ${cid}`);
+  process.stdout.write(`::set-output name=cid::${cid}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
