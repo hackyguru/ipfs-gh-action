d48416b71ec977a0fb90f4c8b4864c69f6afe77f
Guru <19eucs071@skcet.ac.in>
2024-10-12 23:58:16 +0530

    Update backup.js

--- backup.js
+++ backup.js
@@ -1,12 +1,35 @@
 const lighthouse = require('@lighthouse-web3/sdk');
+const { FormData } = require('formdata-node');
+const { fileFromPath } = require('formdata-node/file-from-path');
+const fs = require('fs');
+const path = require('path');
 
 async function main() {
   const apiKey = process.env.LIGHTHOUSE_API_KEY;
-  const uploadResponse = await lighthouse.upload('./', apiKey);
+  
+  const formData = new FormData();
+  
+  // Recursively add all files in the current directory
+  const addFilesToForm = (dir) => {
+    const files = fs.readdirSync(dir);
+    for (const file of files) {
+      const filePath = path.join(dir, file);
+      const stat = fs.statSync(filePath);
+      if (stat.isDirectory()) {
+        addFilesToForm(filePath);
+      } else {
+        formData.append('file', fileFromPath(filePath), file);
+      }
+    }
+  };
+  
+  addFilesToForm('.');
+
+  const uploadResponse = await lighthouse.upload(formData, apiKey);
 
   const cid = uploadResponse.data.Hash;
-  console.log(`Uploaded to IPFS : ${cid}`);
-  process.stdout.write(`::set-output name=cid::${cid}`);
+  console.log(`Uploaded to IPFS: ${cid}`);
+  console.log(`::set-output name=cid::${cid}`);
 }
 
 main().catch((error) => {
